eb0867251ece13912d32e2802f995f7605d8e997
WonYoung Choi <wy80.choi@samsung.com>
2021-09-30 10:11:23 +0900

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,59 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+type State = 'error' | 'failure' | 'pending' | 'success'
+
+function toState(str: string): State {
+  if (
+    str === 'error' ||
+    str === 'failure' ||
+    str === 'pending' ||
+    str === 'success'
+  ) {
+    return str
+  } else {
+    throw new Error(`Invalid state: ${str}`)
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const authToken: string = core.getInput('token', {required: true})
+    const statusContext: string = core.getInput('context', {required: true})
+    const statusState: State = toState(core.getInput('state', {required: true}))
+    const statusDescription: string = core.getInput('description')
+    const statusTargetUrl: string = core.getInput('targetUrl')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const context = github.context
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+    if (context.payload.workflow_run.event !== 'pull_request') {
+      throw new Error(
+        'This action only works on workflow_run triggered by a pull request.'
+      )
+    }
+
+    const octokit = github.getOctokit(authToken)
+
+    for (const pr of context.payload.workflow_run.pull_requests) {
+      if (pr.base.repo.url !== context.payload.repository?.url) {
+        continue
+      }
+      octokit.rest.repos.createCommitStatus({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        sha: pr.head.sha,
+        state: statusState,
+        context: statusContext,
+        description: statusDescription,
+        target_url: statusTargetUrl
+      })
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed(error as string)
+    }
   }
 }
 
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export INPUT_TOKEN='A_TOKEN'
+export INPUT_CONTEXT='A_CONTEXT'
+export INPUT_STATE='invalid'
+
+node lib/main.js
