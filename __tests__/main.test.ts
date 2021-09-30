50632b31346746c9e9c7e915d37a8813d1ca798d
WonYoung Choi <wy80.choi@samsung.com>
2021-09-30 17:28:29 +0900

    Introduce workflow-run-status-action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,55 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test('tests run without token', () => {
+  const result = executeAction()
+  expect(result).toMatch('::error::Input required and not supplied: token')
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test('tests run without context', () => {
+  process.env['INPUT_TOKEN'] = '_A_TOKEN_'
+  const result = executeAction()
+  expect(result).toMatch('::error::Input required and not supplied: context')
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('tests run not on workflow_run', () => {
+  process.env['INPUT_TOKEN'] = '_A_TOKEN_'
+  process.env['INPUT_CONTEXT'] = '_A_CONTEXT_'
+  process.env['GITHUB_EVENT_NAME'] = 'pull_request'
+  const result = executeAction()
+  expect(result).toMatch(
+    '::error::This action can only be used in a workflow_run event'
+  )
+})
+
+test('tests run triggred by push', () => {
+  process.env['INPUT_TOKEN'] = '_A_TOKEN_'
+  process.env['INPUT_CONTEXT'] = '_A_CONTEXT_'
+  process.env['GITHUB_EVENT_NAME'] = 'workflow_run'
+  process.env['GITHUB_EVENT_PATH'] = path.join(
+    __dirname,
+    'payload_triggered_by_push.json'
+  )
+  const result = executeAction()
+  expect(result).toMatch(
+    '::error::This action only works on workflow_run triggered by a pull request.'
+  )
+})
+
+function executeAction() {
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+
+  let result: string = ''
+  try {
+    result = cp.execFileSync(np, [ip], options).toString()
+  } catch (error) {
+    result = error.stdout.toString()
+  }
+  return result
+}
