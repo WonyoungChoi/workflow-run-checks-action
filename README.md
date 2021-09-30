034736062e23156df13ec2e3331501977932e455
WonYoung Choi <wy80.choi@samsung.com>
2021-09-30 17:28:29 +0900

    Introduce workflow-run-status-action

--- README.md
+++ README.md
@@ -1,105 +1,41 @@
-<p align="center">
-  <a href="https://github.com/actions/typescript-action/actions"><img alt="typescript-action status" src="https://github.com/actions/typescript-action/workflows/build-test/badge.svg"></a>
-</p>
+# workflow-run-status-action
 
-# Create a JavaScript Action using TypeScript
+> This action is forked from [haya14busa/action-workflow_run-status](https://github.com/haya14busa/action-workflow_run-status).
 
-Use this template to bootstrap the creation of a TypeScript action.:rocket:
+![image](https://user-images.githubusercontent.com/1029205/135450564-0f965a55-fc11-41ee-ae6d-4637c8c88142.png)
 
-This template includes compilation support, tests, a validation workflow, publishing, and versioning guidance.  
+This action updates the commit status to the pull request that triggered the [workflow_run](https://docs.github.com/en/actions/learn-github-actions/events-that-trigger-workflows#workflow_run).
 
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
-
-## Create an action from this template
-
-Click the `Use this Template` and provide the new repo details for your action
-
-## Code in Main
-
-> First, you'll need to have a reasonably modern version of `node` handy. This won't work with versions older than 9, for instance.
-
-Install the dependencies  
-```bash
-$ npm install
-```
-
-Build the typescript and package it for distribution
-```bash
-$ npm run build && npm run package
-```
-
-Run the tests :heavy_check_mark:  
-```bash
-$ npm test
-
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
-
-...
-```
-
-## Change action.yml
-
-The action.yml contains defines the inputs and output for your action.
-
-Update the action.yml with your name, description, inputs and outputs for your action.
-
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
-
-## Change the Code
-
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
-
-```javascript
-import * as core from '@actions/core';
-...
-
-async function run() {
-  try { 
-      ...
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run()
-```
-
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
-
-## Publish to a distribution branch
-
-Actions are run from GitHub repos so we will checkin the packed dist folder. 
-
-Then run [ncc](https://github.com/zeit/ncc) and push the results:
-```bash
-$ npm run package
-$ git add dist
-$ git commit -a -m "prod dependencies"
-$ git push origin releases/v1
-```
-
-Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
-
-Your action is now published! :rocket: 
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Validate
-
-You can now validate the action by referencing `./` in a workflow in your repo (see [test.yml](.github/workflows/test.yml))
+## Usage
+Just add `WonyoungChoi/workflow-run-status-action@v1` as a first step of workflow jobs triggered by workflow_run event.
 
 ```yaml
-uses: ./
-with:
-  milliseconds: 1000
+name: workflow-run-test
+
+on:
+  workflow_run:
+    workflows: ["build-test"]
+    types:
+      - requested
+      - completed
+
+jobs:
+  test-on-workflow-run:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: WonyoungChoi/workflow-run-status-action@v1
+        with:
+          message_on_pending: 'Waiting for build finish'
+          message_on_success: 'Successfully finished'
+          message_on_failure: 'Failed'
+      ...
 ```
 
-See the [actions tab](https://github.com/actions/typescript-action/actions) for runs of this action! :rocket:
-
-## Usage:
-
-After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
+## Inputs
+- token: GitHub access token. Default is `GITHUB_TOKEN`.
+- context: A displayed commit status context on a pull request.
+- message: A default commit status message if the `message_on_*` is not set.
+- message_on_pending: The commit status message when the `workflow_run` event is triggered by `requested` event.
+- message_on_success: The commit status message when the `workflow_run` event is successfully completed.
+- message_on_failure: The commit status message when the `workflow_run` event is failed.
+- message_on_running: The commit status message when the `workflow_run` event is running.
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,149 @@
+// From: https://github.com/haya14busa/action-workflow_run-status/blob/main/src/main.ts
+
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as stateHelper from './state-helper'
 import {wait} from './wait'
+
+const context = github.context
+
+type State = 'pending' | 'running' | 'failure' | 'success'
+type CommitState = 'error' | 'failure' | 'pending' | 'success'
+
+type JobPayload = {
+  id: number
+  run_id: number
+  node_id: string
+  head_sha: string
+  url: string
+  html_url: string | null
+  status: string
+  conclusion: string | null
+  started_at: string
+  completed_at: string | null
+  name: string
+  steps?: {
+    name: string
+    status: string
+    conclusion: string | null
+    number: number
+    started_at?: string | null | undefined
+    completed_at?: string | null | undefined
+  }[]
+  check_run_url: string
+}
+
+function getState(job?: JobPayload): State {
+  if (context.payload.action === 'requested') {
+    return 'pending'
+  } else if (!stateHelper.IsPost) {
+    return 'running'
+  } else {
+    // Find step with failure instead of relying on job.conclusion because this
+    // (post) action itself is one of a step of this job and job.conclusion is
+    // always null while running this action.
+    const failedStep = job?.steps?.find(step => step.conclusion === 'failure')
+    if (failedStep) {
+      return 'failure'
+    }
+    return 'success'
+  }
+}
+
+function getMessage(status: State): string {
+  const statusMessage: string = core.getInput('message')
+  const statusMessageOnSuccess: string = core.getInput('message_on_success')
+  const statusMessageOnFailure: string = core.getInput('message_on_failure')
+  const statusMessageOnRunning: string = core.getInput('message_on_running')
+  const statusMessageOnPending: string = core.getInput('message_on_pending')
+
+  switch (status) {
+    case 'success':
+      return statusMessageOnSuccess || statusMessage || 'Successfully finished'
+    case 'failure':
+      return statusMessageOnFailure || statusMessage || 'Failed'
+    case 'running':
+      return statusMessageOnRunning || statusMessage || 'In progress'
+    case 'pending':
+      return (
+        statusMessageOnPending || statusMessage || 'Waiting for preceding jobs'
+      )
+    default:
+      return statusMessage
+  }
+}
+
+function toCommitState(state: State): CommitState {
+  switch (state) {
+    case 'success':
+      return 'success'
+    case 'failure':
+      return 'failure'
+    case 'running':
+      return 'pending'
+    case 'pending':
+      return 'pending'
+    default:
+      return 'error'
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const authToken: string = core.getInput('token', {required: true})
+    const statusContext: string = core.getInput('context', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (context.eventName !== 'workflow_run') {
+      throw new Error('This action can only be used in a workflow_run event')
+    }
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+    if (context.payload.workflow_run.event !== 'pull_request') {
+      throw new Error(
+        'This action only works on workflow_run triggered by a pull request.'
+      )
+    }
+
+    if (!context.payload.repository) {
+      throw new Error('Repository is missing.')
+    }
+
+    if (stateHelper.IsPost) {
+      core.info('Waiting 10 secs for other steps job completion.')
+      await wait(10 * 1000)
+    }
+
+    const octokit = github.getOctokit(authToken)
+    const jobs = await octokit.rest.actions.listJobsForWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId,
+      filter: 'latest',
+      per_page: 100
+    })
+    const job = jobs.data.jobs.find(j => j.name === context.job)
+    if (!job) {
+      throw new Error(`job not found: ${context.job}`)
+    }
+
+    const state = getState(job)
+    const repoName = `${context.repo.owner}/${context.repo.repo}`
+    const statusTargetUrl = `https://github.com/${repoName}/actions/runs/${context.runId}`
+
+    octokit.rest.repos.createCommitStatus({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      sha: context.payload.workflow_run.head_sha,
+      state: toCommitState(state),
+      context: statusContext,
+      description: getMessage(state),
+      target_url: statusTargetUrl
+    })
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed(error as string)
+    }
   }
 }
 
